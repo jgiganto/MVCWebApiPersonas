9f00a6ddc0ec5873582f833561009fdda50fa796
jgiganto <jgiganto1@hotmail.com>
2018-03-05 09:28:01 +0100

    Add project files.

---
+++ MVCWebApiPersonas/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MVCWebApiPersonas.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MVCWebApiPersonas/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MVCWebApiPersonas.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
